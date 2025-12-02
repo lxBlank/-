7293cf1275427e6b7db45716bee411d9cb967d1f
shuwu000 <1448278818@qq.com>
2025-12-02 22:30:38 +0800

    添加admin

--- ceshi/admin.py
+++ ceshi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product, Order
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    search_fields = ('name',)
+
+
